60ca7b6651e0e00efd3fff04eaf775add9a69cff
Vivek Baruah <vivekbrh01@gmail.com>
2020-11-17 00:03:11 +0530

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,14 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.new(user_params)
-    if @user.save 
-      #Handle successful save
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
     else
       render "new"
     end
   end
-
+  
   private
 
     def user_params
