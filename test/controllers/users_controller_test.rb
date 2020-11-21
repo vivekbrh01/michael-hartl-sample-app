e41dc7c51dbbb8464d811e6c6b00568399223160
Vivek Baruah <vivekbrh01@gmail.com>
2020-11-21 10:48:53 +0530

    Finish user edit, update, index and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -48,5 +48,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @other_user.admin?
     patch user_path(@other_user), params: { user: { password: "password", password_confirmation: "password", admin: true } }
     assert_not @other_user.reload.admin?
-  end     
+  end    
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in as a non-admin" do
+    log_in_as(@other_user) 
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end 
 end
