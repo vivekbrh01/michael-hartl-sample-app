79f4556a1e406447c589f819bd233107dc36a037
Vivek Baruah <vivekbrh01@gmail.com>
2020-12-02 12:43:27 +0530

    Complete manipulating microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,11 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-  
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   private 
     def downcase_email
       self.email = email.downcase 
